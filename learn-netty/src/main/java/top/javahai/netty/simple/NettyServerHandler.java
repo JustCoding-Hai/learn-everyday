6f05c737dd3e2b6f23670dab8431e4bcafcce36b
hai_global <jinhaihuang824@aliyun.com>
2020-12-13 23:14:08 +0800

    Netty学习之任务队列

--- learn-netty/src/main/java/top/javahai/netty/simple/NettyServerHandler.java
+++ learn-netty/src/main/java/top/javahai/netty/simple/NettyServerHandler.java
@@ -5,6 +5,9 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
+import org.omg.PortableServer.THREAD_POLICY_ID;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Hai
@@ -23,13 +26,44 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         //输出当前线程信息
-        System.out.println(Thread.currentThread().getName());
+        System.out.println("处理当前连接的线程："+Thread.currentThread().getName());
         System.out.println(ctx.channel());
         System.out.println(ctx.pipeline());
         //将msg转为ByteBuf
         ByteBuf buf = (ByteBuf) msg;
         System.out.println("客户端发送来的消息："+buf.toString(CharsetUtil.UTF_8));
         System.out.println("客户端的地址是："+ctx.channel().remoteAddress());
+        ctx.channel().eventLoop().execute(()->{
+            try {
+                System.out.println("Task1执行的线程："+Thread.currentThread().getName());
+                Thread.sleep(10*1000);
+                ctx.writeAndFlush(Unpooled.copiedBuffer("I am  task1",CharsetUtil.UTF_8));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        ctx.channel().eventLoop().execute(()->{
+            try {
+                System.out.println("Task2执行的线程："+Thread.currentThread().getName());
+                Thread.sleep(5*1000);
+                ctx.writeAndFlush(Unpooled.copiedBuffer("I am task2",CharsetUtil.UTF_8));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        //用户自定义定时任务
+        ctx.channel().eventLoop().schedule(()->{
+            try {
+                System.out.println("Task3执行的线程："+Thread.currentThread().getName());
+                Thread.sleep(5*1000);
+                ctx.writeAndFlush(Unpooled.copiedBuffer("Schedule Task",CharsetUtil.UTF_8));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        },5, TimeUnit.SECONDS);
+
     }
 
     /**
@@ -39,9 +73,10 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
      */
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("客户端连接成功！");
         //将数据写入到缓存并刷新,调用Unpooled.copiedBuffer进行重编码
         ctx.writeAndFlush(Unpooled.copiedBuffer("Hello，客户端",CharsetUtil.UTF_8));
-
     }
 
     /**
