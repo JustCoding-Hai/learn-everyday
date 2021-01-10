69b6a1fb9e6351b1f6fc4e3e4605174ee332aacd
hai_global <jinhaihuang824@aliyun.com>
2021-01-10 22:58:16 +0800

    学习阿里云OSS-删除隐私

--- learn-netty/src/main/java/top/javahai/netty/inandoutboundhandler/MyServer.java
+++ learn-netty/src/main/java/top/javahai/netty/inandoutboundhandler/MyServer.java
@@ -4,6 +4,10 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Hai
@@ -13,10 +17,13 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
  **/
 public class MyServer {
     public static void main(String[] args) {
+        Logger logger= LoggerFactory.getLogger(MyServer.class);
         NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
         NioEventLoopGroup workerGroup = new NioEventLoopGroup(8);
         ServerBootstrap serverBootstrap = new ServerBootstrap();
         serverBootstrap.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class)
+                //设置日志输出级别，默认为DeBug
+                .handler(new LoggingHandler(LogLevel.ERROR))
                 .childHandler(new MyServerInitializer());
         try {
             ChannelFuture channelFuture = serverBootstrap.bind(7000).sync();
--- learn-oss/src/test/java/OssTest.java
+++ learn-oss/src/test/java/OssTest.java
@@ -16,8 +16,8 @@ public class OssTest {
     // Endpoint以杭州为例，其它Region请按实际情况填写。
     String endpoint = "http://oss-cn-beijing.aliyuncs.com";
     // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-    String accessKeyId = "LTAI4Fz2PZAZCvWCtNRayByB";
-    String accessKeySecret = "4mqfKHagS1K9Sm7hQXTppRA6rtZc2S";
+    String accessKeyId = "<accessKeyId>";
+    String accessKeySecret = "<accessKeySecret>";
     String bucketName = "admin-test123";
 
     /**
