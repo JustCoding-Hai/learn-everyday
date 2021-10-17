9ba1e4708ad0074f63039cf36ef16531075227fa
hai_global <jinhaihuang824@aliyun.com>
2021-10-18 00:41:27 +0800

    学习JVM相关的直接内存部分的DENO

---
+++ learn-jvm/src/main/java/heap/Account.java
@@ -0,0 +1,10 @@
+package heap;
+
+/**
+ * @author Hai
+ * @program: learn-everyday
+ * @description:
+ * @create 2021/10/17 - 0:19
+ **/
+public class Account {
+}
---
+++ learn-jvm/src/main/java/heap/Customer.java
@@ -0,0 +1,20 @@
+package heap;
+
+/**
+ * @author Hai
+ * @program: learn-everyday
+ * @description:
+ * @create 2021/10/17 - 0:19
+ **/
+public class Customer {
+    int id = 1001;
+    String name;
+    Account acct;
+
+    {
+        name = "匿名客户";
+    }
+
+    public Customer() {
+    }
+}
