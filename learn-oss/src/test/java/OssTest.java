a139595eedef6a8ece0cd95ea58b28da0c76fba4
hai_global <jinhaihuang824@aliyun.com>
2021-12-06 00:09:20 +0800

    oss补充学习

--- learn-oss/src/test/java/OssTest.java
+++ learn-oss/src/test/java/OssTest.java
@@ -1,8 +1,12 @@
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
 import com.aliyun.oss.model.CannedAccessControlList;
 import com.aliyun.oss.model.CreateBucketRequest;
+import com.aliyun.oss.model.OSSObject;
 import org.junit.Test;
+
+import java.io.*;
 
 /**
  * @author Hai
@@ -45,8 +49,11 @@ public class OssTest {
         ossClient.shutdown();
     }
 
+    /**
+     * 2. 测试存储空间访问权限
+     */
     @Test
-    public void testAccessControl(){
+    public void testAccessControl() {
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
@@ -55,6 +62,91 @@ public class OssTest {
         ossClient.shutdown();
 
     }
+
+
+    /**
+     * 3. 上传文件
+     */
+    @Test
+    public void testFileUpload() {
+        // 填写文件名。文件名包含路径，不包含Bucket名称。例如exampledir/exampleobject.txt。
+        //group-chat对应bucket中的目录名
+        String objectName = "group-chat/exampleobject.txt";
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+
+            String content = "Hello OSS";
+            ossClient.putObject(bucketName, objectName, new ByteArrayInputStream(content.getBytes()));
+        } catch (OSSException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭OSSClient。
+            ossClient.shutdown();
+        }
+    }
+
+
+    /**
+     * 4. 下载文件
+     */
+    @Test
+    public void testFileDownload() {
+        // 填写文件名。文件名包含路径，不包含Bucket名称。例如exampledir/exampleobject.txt。
+        //group-chat对应bucket中的目录名
+        String objectName = "group-chat/exampleobject.txt";
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+
+            // 调用ossClient.getObject返回一个OSSObject实例，该实例包含文件内容及文件元信息。
+            OSSObject ossObject = ossClient.getObject(bucketName, objectName);
+            // 调用ossObject.getObjectContent获取文件输入流，可读取此输入流获取其内容。
+            InputStream content = ossObject.getObjectContent();
+            if (content != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                while (true) {
+                    String line = reader.readLine();
+                    if (line == null) break;
+                    System.out.println("\n" + line);
+                }
+                // 数据读取完成后，获取的流必须关闭，否则会造成连接泄漏，导致请求无连接可用，程序无法正常工作。
+                content.close();
+            }
+        } catch (OSSException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭OSSClient。
+            ossClient.shutdown();
+        }
+    }
+
+
+    /**
+     * 5.删除文件
+     */
+    @Test
+    public void testFileDelete() {
+        // 填写文件名。文件名包含路径，不包含Bucket名称。例如exampledir/exampleobject.txt。
+        //group-chat对应bucket中的目录名
+        String objectName = "group-chat/exampleobject.txt";
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+
+            // 删除文件。
+            ossClient.deleteObject(bucketName, objectName);
+        } catch (OSSException e){
+            e.printStackTrace();
+        } finally {
+            // 关闭OSSClient。
+            ossClient.shutdown();
+        }
+    }
+
 
 
 }
